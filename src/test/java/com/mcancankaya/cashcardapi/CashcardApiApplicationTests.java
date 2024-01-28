05fb2d8f36f4fa58d015109fcda56f9c7980a21a
mcancankaya <mccankaya45@hotmail.com>
2024-01-28 13:27:01 +0300

    init commit

---
+++ src/test/java/com/mcancankaya/cashcardapi/CashcardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcancankaya.cashcardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcardApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
